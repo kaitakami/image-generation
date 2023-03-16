cc3caffca36d1d8dc1234cf525f579904844c323
Kai Takami <kaitakami@protonmail.com>
2023-03-16 14:27:15 +0900

    Remove Image to check production

--- app/api/thumbnail/route.tsx
+++ app/api/thumbnail/route.tsx
@@ -50,17 +50,7 @@ export async function GET(req: NextRequest) {
           }}
           className='rounded-full'
         />
-        <img
-          src={imageUrl}
-          style={{
-            position: 'absolute',
-            bottom: 0,
-            right: 60,
-            height: '600',
-            zIndex: -1,
-          }}
-          alt="Foto del invitadx"
-        />
+
         <div
           style={{
             display: 'flex'
--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           <div>
             <label htmlFor="image" className="flex mb-2 text-sm font-medium text-gray-900 dark:text-white">URL de la imagen<Link href="https://postimages.org/" target="_blank" className="my-auto ml-auto text-xs font-normal underline hover:text-blue-800 dark:hover:text-blue-200">Sube tu imagen y pega la URL aquí</Link></label>
             <input type="url" name="image" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="https://i.postimg.cc/HWKRrWPS/fundadores-logo.png"
-              onChange={handleInputChange} value={guest.image} required />
+              onChange={handleInputChange} value={guest.image} />
           </div>
           <div>
             <label htmlFor="result" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Resultado</label>
